1248f1332cd1fc7dc9a4f3fc083fc60d5336f7d7
Abigayle Franklin <abifranklin@gmail.com>
2019-04-30 15:55:19 -0500

    Added route transitions

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,19 +1,65 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Nav from './Components/Nav'
 import About from './Components/About'
 import Portfolio from './Components/Portfolio'
 import Contact from './Components/Contact'
+import { spring, AnimatedSwitch } from 'react-router-transition';
 
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 15,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(1),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
 
 function App() {
   return (
     <div className="App">
+
+   
       <Nav />
+      <AnimatedSwitch
+    atEnter={bounceTransition.atEnter}
+    atLeave={bounceTransition.atLeave}
+    atActive={bounceTransition.atActive}
+    mapStyles={mapStyles}
+    className="route-wrapper"
+  >
       <Route path="/" exact component={About} />
       <Route path="/portfolio" component={Portfolio} />
       <Route path="/contact" component={Contact} />
+      </AnimatedSwitch>
+
     </div>
   );
 }
